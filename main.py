564eb83b3f48b878c4b23def213f010acb8d4c20
Abdullah Yusuf Yıldırım <tt.shirted@gmail.com>
2026-01-28 20:46:14 +0300

    key label ,key entry,Encrypt Button,Decrypt Button added

--- main.py
+++ main.py
@@ -41,6 +41,30 @@ Secret Text
 secret_text = tkinter.Text(bg = "white",fg = "black",highlightbackground = "light grey",insertbackground="blue",highlightthickness=0)
 secret_text.place(width = 350 , x = 30 , y = 215)
 
+"""
+Enter Master Key Label 
+"""
+master_key_label = tkinter.Label(text = "Enter Master Key", fg = "black",bg = "light grey", font = ("Arial",16,))
+master_key_label.place(x = 145 , y = 540)
+"""
+Master Key Entry
+"""
+master_key_entry = tkinter.Entry(relief="flat",borderwidth=0,fg = "black", bg = "white",highlightbackground = "light grey",insertontime=100,
+                                 insertbackground="blue",highlightthickness=0)
+master_key_entry.place(width = 300 ,x = 58,y = 570)
+"""
+Save & Encrypt Button
+"""
+save_and_encrypt_button = tkinter.Button(text = "Save & Encrypt", fg = "black", bg = "light grey",font = ("Arial,12")
+                                         ,width = 12,highlightthickness=0,highlightbackground = "light grey")
+save_and_encrypt_button.place(x = 140 , y = 605)
+
+"""
+Decrypt Button 
+"""
+decrypt_button = tkinter.Button(text = "Decrypt", fg = "black", bg = "light grey",font = ("Arial,10")
+                                ,relief="flat",borderwidth=0 ,overrelief="ridge",width = 8,highlightbackground = "light grey")
+decrypt_button.place(x = 162 , y = 640)
 
 
 
